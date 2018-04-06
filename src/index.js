7ddff5faf8ea27a15431e185bde4c72be3fd779f
Leonardo Faria <leonardofaria@gmail.com>
2018-04-06 11:13:08 -0700

    Implement Code Splitting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import foo from "./foo";
 import {a} from "./bar";
 import imgSrc from "./icon-square-small.jpg";
 import "./index.css";
+// import foo from "./foo";
+const foo = () => import("./foo");
 
 console.log(imgSrc)
 
 const button = document.createElement("button");
 button.innerText = "BUTTON";
-debugger
 const fancyImage = document.createElement("img");
 fancyImage.src = imgSrc;
 
 document.body.appendChild(button);
 document.body.appendChild(fancyImage);
+
+button.addEventListener("click", e => {
+  foo().then(module => {
+    // You can use the module
+    console.log(module.default);
+    // debugger;
+  })
+});
