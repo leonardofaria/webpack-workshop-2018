43c5d39d8523ed64ffd46b365e347e5d77f19e23
Leonardo Faria <leonardofaria@gmail.com>
2018-04-06 11:16:20 -0700

    Implement Code Splitting - Part II

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {a} from "./bar";
 import imgSrc from "./icon-square-small.jpg";
 import "./index.css";
 // import foo from "./foo";
-const foo = () => import("./foo");
+const foo = () => import("./foo").then(m => m.default); // Use the default export when the promise is resolved
 
 console.log(imgSrc)
 
@@ -17,7 +17,7 @@ document.body.appendChild(fancyImage);
 button.addEventListener("click", e => {
   foo().then(module => {
     // You can use the module
-    console.log(module.default);
+    console.log(module); // [1, 3, 4, 5, 6, 7]
     // debugger;
   })
 });
